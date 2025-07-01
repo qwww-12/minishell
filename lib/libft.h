523cf864027478c6eb5275c6a1d98752d962eda5
Mohamed Ayoub Barhoun <mbarhoun@c1r4p5.1337.ma>
2025-07-01 21:57:26 +0100

    when clone change name libft_ as libft

---
+++ lib/libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarhoun <mbarhoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 20:50:15 by mbarhoun          #+#    #+#             */
+/*   Updated: 2025/06/22 19:46:19 by mbarhoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_strncpy(char *s1, char *s2, int n);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_itoa(int n);
+int		ft_strlen(const char *s);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_strcmp(const char *s1, const char *s2);
+
+#endif
