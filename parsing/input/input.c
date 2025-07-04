a83e79bff6f3fc08cdd9823d25b980bac3e0230f
Mohamed Ayoub Barhoun <mbarhoun@c1r4p9.1337.ma>
2025-07-04 08:18:43 +0100

    update

--- parsing/input/input.c
+++ parsing/input/input.c
@@ -6,26 +6,11 @@
 /*   By: mbarhoun <mbarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 20:17:36 by mbarhoun          #+#    #+#             */
-/*   Updated: 2025/06/02 16:36:34 by mbarhoun         ###   ########.fr       */
+/*   Updated: 2025/07/04 08:15:57 by mbarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-static bool	is_spaces(char *input)
-{
-	int	r;
-	int	spc;
-
-	r = -1;
-	spc = 0;
-	while (input[++r])
-		if (is_space(input[r]))
-			spc++;
-	if (spc == ft_strlen(input))
-		return (1);
-	return (0);
-}
 
 void	glance_input(char *input, t_env *env)
 {
@@ -37,7 +22,7 @@ void	glance_input(char *input, t_env *env)
 		printf("exit\n");
 		exit(0);
 	}
-	if (input[0] == '\0' || is_spaces(input))
+	if (input[0] == '\0')
 		return ;
 	add_history(input);
 }
